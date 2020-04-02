d2c57b63b741814fd69c77975f06dada10c77eec
hinameplays <heinrich-karow@gmx.de>
2020-04-02 11:30:30 +0200

    f* greenfoot

---
+++ src/Block.java
@@ -0,0 +1,17 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Block here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Block extends Actor
+{
+    int type;
+    int ID;
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
---
+++ src/Listener.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Makes em'
+ * Shakes em'
+ * Destroys em'
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Listener extends World
+{
+    
+    public Listener()
+    {   
+        //creates new listener w/ greenfoot propertys
+        super(400, 600, 1); 
+        
+    }
+    
+}
